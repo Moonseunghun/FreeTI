7a3b9bb96fcf46730e2cad6eacdc976e8d9e33f7
moonseunghun <mangokkaksae2@gmail.com>
2023-10-09 16:47:04 +0900

    Feat : provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,63 @@
-class Monda extends StatelessWidget {
-  const Monda({super.key});
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class CounterProvider with ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: ChangeNotifierProvider(
+        create: (context) => CounterProvider(),
+        child: CounterApp(),
+      ),
+    );
+  }
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final counterProvider = Provider.of<CounterProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Count:',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              '${counterProvider.count}',
+              style: TextStyle(fontSize: 36),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterProvider.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
